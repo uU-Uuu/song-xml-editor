ee9f11cf80a80c23f42c4a75bae38c5428d7df20
uU-Uuu <ustja.54@gmail.com>
2024-07-19 15:34:00 +0300

    implemented creating, saving, editing xml file backbone

--- assist.py
+++ assist.py
@@ -2,7 +2,7 @@ import re
 
 
 FILENAME_REQUIREMENTS = """\t - starting from lowercase or uppercase alphabetic characters
-\t - allowed: a-z A-Z 0-9 _\n\t - extension: optional"""
+\t - allowed: a-z A-Z 0-9 _\n\t - extension: optional"\n\t - maximum length: 255 characters"""
 
 
 class InvalidFilename(Exception):
@@ -12,26 +12,4 @@ class InvalidFilename(Exception):
         super().__init__(self.message)
 
 
-
-def filename_validation(filename, pattern=r'(?=^[a-zA-Z]\w+)(\w+)(\.xml)?$', extension='.xml'):
-    """validate filename with regex pattern and extension
-    
-    Parameter:
-    - filename (str): Filename to be validated 
-    - pattern (str, optional): regex r'' pattern
-        - default r'(?=^[a-zA-Z]\w+)(\w+)(\.xml)?$'
-    - extension (str, optional): Indended file extension
-        - default '.xml'
-
-    """
-    check_filename = re.match(pattern, filename)
-    if check_filename:
-        if check_filename.group(2) == extension:
-            valid_filename = filename
-        else:
-            valid_filename = filename + extension
-    else:
-        raise InvalidFilename(f'Invalid filename: {filename}\n\tRequirements:\n{FILENAME_REQUIREMENTS}')
-    return valid_filename
-
-        
+   
--- doc-structure.py
+++ doc-structure.py
@@ -1,7 +1,8 @@
 import os
+import re
 
 
-from assist import filename_validation, InvalidFilename
+from assist import InvalidFilename, FILENAME_REQUIREMENTS
 
 
 class Workspace:
@@ -10,13 +11,17 @@ class Workspace:
     Attributes:
     - name (str): the workspace name
     - base_directory (str): the base directory where documents will be saved
+    - documents (list): a list of XMLDocuments instances in the workspace
     
     Methods:
-    - __init__(self, name, base_directory)"""
+    - __init__(self, name, base_directory)
+    - add_document(self, filename)
+      initializes new XMLDoc instance and appends it to the self.documents attribute"""
 
     def __init__(self, name, base_directory):
         self._name = name
         self._base_directory = base_directory
+        self._documents = []
 
     @property
     def name(self):
@@ -30,6 +35,29 @@ class Workspace:
     @property
     def base_directory(self):
         return self._base_directory
+    
+    @base_directory.setter
+    def base_directory(self, new_directory):
+        if new_directory:
+            self._base_directory = new_directory
+    
+    @property
+    def documents(self):
+        return self._documents
+    
+    @documents.setter
+    def documents(self, documents_list):
+        if documents_list:
+            self._documents = documents_list
+    
+
+
+    def add_document(self, filename, path=''):
+        full_path = os.path.join(self._base_directory, path)
+        if not os.path.exists(full_path):
+            os.makedirs(full_path)
+        new_document = XMLDoc(filename, full_path)
+        self.documents.append(new_document)
 
 
 class XMLDoc:
@@ -38,35 +66,122 @@ class XMLDoc:
     Attributes:
     - filename (str)
     - path (str): relative path
-    - title (str): document title
-    - key (str): melody key value
-    - time_signature (str): time signature value 
+    - title (str, optional): document title
+    - key (str, optional): melody key value
+    - time_signature (str, optional): time signature value 
     
     Methods:
-    - __init__(self, filename, path)
+    - __init__(self, filename, path, title, key, time_signature)
     - create(self)
       creates and saves the document backbone to self.path in the workspace directory
     """
 
-    def __init__(self, filename, path):
-        self._filename = filename
-        self._path = path
-        self._title = None
-        self._key = None
-        self._time_signature = None
+    def __init__(self, filename, path, title='', key='', time_signature=''):
+        self.filename = filename
+        self.path = path
+        self.title = title
+        self.key = key
+        self.time_signature = time_signature
 
 
-    def create(self):
+    @staticmethod
+    def filename_validation(filename_to_check, pattern=r'(?=^[a-zA-Z]\w+)(\w+){1,255}(\.xml)?$', extension='.xml', check=False):
+        """a static method to validate filename with regex pattern and file extension
+        
+        Parameters:
+        - filename (str): Filename to be validated 
+        - pattern (str, optional): regex r'' pattern
+            - default r'(?=^[a-zA-Z]\w+)(\w+)(\.xml)?$'
+        - extension (str, optional): Indended file extension
+            - default '.xml'
+        - check (bool): when True returns True if the filename is valid else InvalidFilename Exception
+            - default False
+        
+        Output:
+        - when check=False:  valid filename (str) or InvalidFilename Exception
+        - when check=True:   True if valid filename or InvalidFilename Exception
+        """
+        check_filename = re.match(pattern, filename_to_check)
+        if check_filename:
+            if check_filename.group(2) == extension:
+                if check:
+                    return True
+                valid_filename = filename_to_check
+            else:
+                valid_filename = filename_to_check + extension
+        else:
+            raise InvalidFilename(f'Invalid filename: {filename_to_check}\n\tRequirements:\n{FILENAME_REQUIREMENTS}')
+        return valid_filename
+    
+
+    def create(self, non_empty=False):
+        """create and save an xml file with metadata backbone
+        
+        Parameters:
+        - non_empty (bool): when True does not include empty title, key, time_signature in the file
+            - default False
+        """
         try:
-            self._filename = filename_validation(self._filename)
-            self._path = os.path.join(self._path, self._filename)
-            try:
-                with open(self._path, 'w') as file:
-                    pass
-            except IOError as _:
-                print(f'Error when trying to save file\n\t {_}')
+            if XMLDoc.filename_validation(self.filename, check=True):
+                self.path = os.path.join(self.path, self.filename)
+                try:
+                    with open(self._path, 'w') as file:
+                        file.write(self.write_metadata_xml(non_empty=non_empty))
+                except IOError as _:
+                    print(f'Error when trying to save file\n\t {_}')
         except InvalidFilename as e:
             print(e)
+
+
+    def set_metadata(self, title:str='', key:str='', time_signature:str=''):
+        """set title, key and time_signature parameters
+        
+        Parameters:
+        - title (str, optional): default ''
+        - key (str, optional): default ''
+        - time_signature (str, optional): default ''
+        """
+        self.title = title if title else self.title
+        self.key = key if key else self.key
+        self.time_signature = time_signature if time_signature else self.time_signature
+    
+
+    def write_metadata_xml(self, version='1.0', encoding='UTF-8', non_empty=False):
+        """create a metadata xml string
+        
+        Parameters:
+        - version (str): default '1.0'
+        - encoding (str): default 'UTF-8'
+        - non-empty (bool): when True does not include attributes (tags) with empty values 
+            - default False
+        """
+        xml_string = f'<?xml version="{version}" encoding="{encoding}"?>\n'
+        for attr, value in vars(self).items():
+            if not attr.startswith('_'):
+                if non_empty:
+                    if value:
+                        xml_string += f'<{attr}>{value}</{attr}>\n' 
+                else:
+                    xml_string += f'<{attr}>{value}</{attr}>\n'
+        return xml_string
+    
+
+    def update_file(self, title:str='', key:str='', time_signature:str=''):
+        """overwrite the file with the current state of the object
+        
+         Parameters:
+        - title (str, optional): default ''
+        - key (str, optional): default ''
+        - time_signature (str, optional): default ''
+        """
+        try:
+            if title or key or time_signature:
+                self.set_metadata(title, key, time_signature)
+            with open(self._path, 'w') as file:
+                file.write(self.write_metadata_xml())
+        except IOError as _:
+                print(f'Error when trying to update the file\n\t {_}')
+
 
 
     @property
@@ -78,52 +193,34 @@ class XMLDoc:
     def filename(self, new_filename:str):
         """document filename setter"""
         try:
-            self._filename = filename_validation(str(new_filename))
+            self._filename = XMLDoc.filename_validation(str(new_filename))
         except InvalidFilename as _:
             print(f'Cannot change the filename. {_}')
     
     @property
     def path(self):
         """document path getter"""
-        return self._path        
-    
-    @property
-    def title(self):
-        """document title getter"""
-        return self._title
-    
-    @title.setter
-    def title(self, new_title:str):
-        """document title setter"""
-        if new_title:
-            self._title = new_title
-    
-    @property
-    def key(self):
-        return self._key
-    
-    @key.setter
-    def key(self, new_key:str):
-        """document key setter"""
-        if new_key:
-            self._key = str(new_key)
-    
-    @property
-    def time_signature(self):
-        """document time_signature getter"""
-        return self._time_signature
-    
-    @time_signature.setter
-    def time_signature(self, new_time_signature):
-        """document time_signature getter"""
-        if new_time_signature:
-            self._time_signature = new_time_signature
+        return self._path 
+
+    @path.setter
+    def path(self, new_path:str):
+        """document path setter"""
+        if new_path:
+            self._path = str(new_path)
 
 
-workspace = Workspace('try', os.getcwd())
-# print(workspace._base_directory)
+workspace = Workspace('current_workspace', os.getcwd())
+
+workspace.add_document('myxml', 'f')
+for doc in workspace.documents:
+    doc.create()
+    doc.set_metadata('ishikaribanka', 'A minor', '4/4')
+    doc.update_file(title='A')
 
 
-xml = XMLDoc('myxml.xml', 'f')
-xml.filename = '67'
-print(xml.filename)
+
+# xml = XMLDoc('myxml', 'f')
+# xml.filename = '67'
+# print(xml.filename)
+
+
