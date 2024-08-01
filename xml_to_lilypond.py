8a3acc3bf2a5a2effb65ae43664cd8116d717220
uU-Uuu <ustja.54@gmail.com>
2024-08-01 16:36:21 +0300

    organized the xml_to_lilypond module functions into XMLToLily class

--- xml_to_lilypond.py
+++ xml_to_lilypond.py
@@ -4,120 +4,117 @@ import os
 import re
 
 
-OCTAVES = {3: "",
+class XMLToLily:
+    _OCTAVES = {3: "",
            4: "'",
            5: "''"}
-
-xml_filename = 'try/tryxml.xml'
-
-
-def parseXML(xml_file):
-    def get_root(func):
-        def inner(*args, **kwargs):
-            tree = ET.parse(xml_file)
-            root = tree.getroot()
-            res = func(root, *args, **kwargs)
-            return res
-        return inner
-    return get_root
     
+    def __init__(self, xml_file):
+        self.xml_file = xml_file
+        self.root = self._get_root()
+   
 
-def note_formatter(raw_pitch, raw_duration):
-    pitch, octave = re.match(r'(\w+)([0-9]$)', raw_pitch).groups()
-    pitch_o = pitch.lower() + OCTAVES[int(octave) - 1]
-    return pitch_o + duration_formatter(raw_duration, rest=False)
+    @classmethod
+    def _note_formatter(cls, raw_pitch, raw_duration):
+        pitch, octave = re.match(r'(\w+)([0-9]$)', raw_pitch).groups()
+        pitch_o = pitch.lower() + cls._OCTAVES[int(octave) - 1]
+        return pitch_o + cls._duration_formatter(raw_duration, rest=False)
 
-
-def duration_formatter(raw_duration, rest=True):
-    numer, denom  = list(map(int, raw_duration.split('/')))
-    dotted = numer == 3 and (denom & (denom - 1)) == 0
-    duration = f'{denom}{["", "."][dotted]}'
-    return f'{["", "r"][rest]}{duration}'
-
-
-@parseXML(xml_filename)
-def parse_xml_meta(root):
-    meta = './/title', './/time_signature'
-    title, time_s = (root.find(tag).text for tag in meta)
-    key, mode = root.find('.//key').text.lower().split()
-    return title, time_s, key, mode
-
-
-def parse_xml_lex_phr(mel_phr):
-    for lex_phr in mel_phr.findall('.//lexical_phrase'):
-        lex_notes = []
-
-        for syllable in lex_phr.findall('.//syllable'):
-            lyric = syllable.find('.//lyric').text
-            notes_raw = list(zip(
-                (pitch.text for pitch in syllable.findall('.//pitch')),
-                (dur.text for dur in syllable.findall('.//duration'))
-            ))
-            lex_notes.append(' '.join(note_formatter(p, d) 
-                                    for (p, d) in notes_raw))
-                    
-        for rest in lex_phr.findall('.//rest'):
-            for duration in rest.findall('.//duration'):
-                lex_notes.append(duration_formatter(duration.text))
-
-        return ' '.join(lex_notes)
+    @classmethod
+    def _duration_formatter(cls, raw_duration, rest=True):
+        numer, denom  = list(map(int, raw_duration.split('/')))
+        dotted = numer == 3 and (denom & (denom - 1)) == 0
+        duration = f'{denom}{["", "."][dotted]}'
+        return f'{["", "r"][rest]}{duration}'
     
+    
+    def _get_root(self):
+        tree = ET.parse(self.xml_file)
+        root = tree.getroot()
+        return root
 
-def parse_xml_mel_phr(section, relative="c"+OCTAVES[4]):
-    time_s, key, mode, = parse_xml_meta()[1:]
-    for mel_phr in section.findall('.//melodic_phrase'):
-        piece = f'Mel. phrase: {mel_phr.attrib["id"]}'
-        mel_notes = []
-        mel_notes.append(parse_xml_lex_phr(mel_phr))
-        mel_phr_piece = ' |\n'.join(mel_notes)
-        mel_phr_str = f"""
+
+    def _parse_xml_meta(self):
+        meta = './/title', './/time_signature'
+        title, time_s = (self.root.find(tag).text for tag in meta)
+        key, mode = self.root.find('.//key').text.lower().split()
+        return title, time_s, key, mode
+
+
+    def _parse_xml_lex_phr(self, mel_phr):
+        for lex_phr in mel_phr.findall('.//lexical_phrase'):
+            lex_notes = []
+
+            for syllable in lex_phr.findall('.//syllable'):
+                lyric = syllable.find('.//lyric').text
+                notes_raw = list(zip(
+                    (pitch.text for pitch in syllable.findall('.//pitch')),
+                    (dur.text for dur in syllable.findall('.//duration'))
+                ))
+                lex_notes.append(' '.join(self._note_formatter(p, d) 
+                                        for (p, d) in notes_raw))
+                        
+            for rest in lex_phr.findall('.//rest'):
+                for duration in rest.findall('.//duration'):
+                    lex_notes.append(self._duration_formatter(duration.text))
+            return ' '.join(lex_notes)
+        
+
+    def _parse_xml_mel_phr(self, section, relative="c"+_OCTAVES[4]):
+        time_s, key, mode, = self._parse_xml_meta()[1:]
+        for mel_phr in section.findall('.//melodic_phrase'):
+            piece = f'Mel. phrase: {mel_phr.attrib["id"]}'
+            mel_notes = []
+            mel_notes.append(self._parse_xml_lex_phr(mel_phr))
+            mel_phr_piece = ' |\n'.join(mel_notes)
+            mel_phr_str = f"""
 \\score {{
     \\header {{
-      piece = "{piece}"
+    piece = "{piece}"
     }}
     \\new Staff \\relative {relative} {{
-      \\key {key} \\{mode}
-      \\time {time_s}
-      {mel_phr_piece} |
+    \\key {key} \\{mode}
+    \\time {time_s}
+    {mel_phr_piece} |
     }}
 }}
-"""
-        return mel_phr_str
+    """
+            return mel_phr_str
 
 
 
-@parseXML(xml_filename)
-def parse_xml_section(root):
-    for section in root.findall('.//section'):
-        subtitle = f'Section: {section.attrib["name"]}'
-    section_str = f"""
+    def _parse_xml_section(self):
+        for section in self.root.findall('.//section'):
+            subtitle = f'Section: {section.attrib["name"]}'
+        section_str = f"""
 \\markup {{
-  \\column {{
+\\column {{
     \\line {{ \\bold "{subtitle}" }}
     }}
 }}
-{parse_xml_mel_phr(section)}
-"""
-    return section_str
+{self._parse_xml_mel_phr(section)}
+    """
+        return section_str
+
 
     
-def xml_to_lily(filename):
-    global xml_filename
-    xml_filename = filename
-    title = parse_xml_meta()[0]
-    lily_script = f"""
-\\version "2.24.4"
-\\header {{
-    title = "{title}"
-}}
-{parse_xml_section()}
-"""
-    return lily_script
+    def xml_to_lily(self):
+        title = self._parse_xml_meta()[0]
+        lily_script = f"""
+    \\version "2.24.4"
+    \\header {{
+        title = "{title}"
+    }}
+    {self._parse_xml_section()}
+    """
+        return lily_script
 
 
 
-xml_filename = 'try/tryxml.xml'
+
 
 
 if __name__ == '__main__':
     xml_filename = 'try/tryxml.xml'
+    converter = XMLToLily(xml_filename)
+    print(converter.xml_to_lily())
